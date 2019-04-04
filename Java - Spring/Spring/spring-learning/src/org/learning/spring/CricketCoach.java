13b537104f24c164a76c50ea893a3e08c1646bec
szymszp563 <szymon.szpakowski6@gmail.com>
2019-04-05 01:36:36 +0200

    Injecting Literal Values

--- Java - Spring/Spring/spring-learning/src/org/learning/spring/CricketCoach.java
+++ Java - Spring/Spring/spring-learning/src/org/learning/spring/CricketCoach.java
@@ -3,16 +3,39 @@ package org.learning.spring;
 public class CricketCoach implements Coach {
 
 	private FortuneService fortuneService;
+	// add new fields for email and team
+	private String emailAddress;
+	private String team;
 	
 	// create a no-arg constructor
 	public CricketCoach() {
 		System.out.println("CricketCoach: inside no-arg constructor");
 	}	
 	
-	// our setter method
-	public void setFortuneService(FortuneService fortuneService) {
-		System.out.println("CricketCoach: inside setter method - setFotuneService");
-		this.fortuneService = fortuneService;
+	
+	// getter methods
+	public String getEmailAddress() {
+		return emailAddress;
+	}
+
+	public String getTeam() {
+		return team;
+	}
+	
+	// setter methods
+		public void setFortuneService(FortuneService fortuneService) {
+			System.out.println("CricketCoach: inside setter method - setFotuneService");
+			this.fortuneService = fortuneService;
+		}
+
+	public void setEmailAddress(String emailAddress) {
+		System.out.println("CricketCoach: inside setter method - setEmailAddress");
+		this.emailAddress = emailAddress;
+	}
+
+	public void setTeam(String team) {
+		System.out.println("CricketCoach: inside setter method - setTeam");
+		this.team = team;
 	}
 
 	@Override
