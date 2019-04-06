2feeaf5ef56a50981069913ce3b80e8c54ec9f25
szymszp563 <szymon.szpakowski6@gmail.com>
2019-04-06 16:03:25 +0200

    value injections

--- Java - Spring/Spring/spring-learning/src/org/learning/spring/HappyFortuneService.java
+++ Java - Spring/Spring/spring-learning/src/org/learning/spring/HappyFortuneService.java
@@ -1,11 +1,23 @@
 package org.learning.spring;
 
+import java.util.Random;
+
 public class HappyFortuneService implements FortuneService {
+	
+	private String[] fortunes;
+		
+	public String[] getFortunes() {
+		return fortunes;
+	}
+
+	public void setFortunes(String[] fortunes) {
+		System.out.println("HappyFortuneService: inside setter method - setFortunes");
+		this.fortunes = fortunes;
+	}
 
 	@Override
 	public String getFortune() {
-
-		return "Today is your lucky day!";
+		return fortunes[(new Random().nextInt(3))];
 	}
 
 }
